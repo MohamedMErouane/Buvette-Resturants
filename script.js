8ae48c5f81eb70f99558dee1fad089f9750b1aa4
Mohamed <merouanemohamed495@gmail.com>
2024-04-05 22:01:53 +0000

    Full Restaurants Website

--- script.js
+++ script.js
@@ -1,9 +1,27 @@
-function login() {
-    var randomNumber = Math.floor(Math.random() * 100) + 1;
-    let name =document.getElementById('name');
-    let number = document.getElementById('number');
-    let p=document.getElementById('p11');
-    p.innerHTML= (name.value)+"\t"+ number.value+ "<br>"+"  Your Turn is: " + randomNumber +"<br>"+Date();
-  }
-  
+let navbar = document.querySelector('.header .flex .navbar');
+let profile = document.querySelector('.header .flex .profile');
 
+document.querySelector('#menu-btn').onclick = () =>{
+   navbar.classList.toggle('active');
+   profile.classList.remove('active');
+}
+
+document.querySelector('#user-btn').onclick = () =>{
+   profile.classList.toggle('active');
+   navbar.classList.remove('active');
+}
+
+window.onscroll = () =>{
+   profile.classList.remove('active');
+   navbar.classList.remove('active');
+}
+
+function loader(){
+   document.querySelector('.loader').style.display = 'none';
+}
+
+function fadeOut(){
+   setInterval(loader, 2000);
+}
+
+window.onload = fadeOut;
